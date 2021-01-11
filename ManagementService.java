dd15964c48ad92acd5b43ee522008751f7f8f717
Arihant Jain <arijain34@gmail.com>
2021-01-11 14:01:44 +0530

    Updated test cases

--- ManagementService.java
+++ ManagementService.java
@@ -8,32 +8,33 @@ import java.util.Map;
 public class ManagementService {
 
     public List<String> getListOfMeetings(User user){
-            List<String> resultList = new ArrayList<>();
-            Map<String, Meeting> userMeetings = user.getMeetings();
-            for(Meeting meetings: userMeetings.values()){
-                resultList.add(meetings.getName());
-            }
-            return resultList;
+        List<String> resultList = new ArrayList<>();
+        Map<String, Meeting> userMeetings = user.getMeetings();
+        for(Meeting meetings: userMeetings.values()){
+            resultList.add(meetings.getName());
+        }
+        return resultList;
     }
 
     public boolean addMeeting(User user, Meeting meeting){
         String meetingName = meeting.getName();
-        String collisionMeetingName = "";
-        boolean dateCollides = false;
+        List<String> meetingsWithcollisonTime = new ArrayList<>();
         Map<String, Meeting> meetings = user.getMeetings();
         if(!meetings.containsKey(meetingName)){
             for(String mNames: meetings.keySet()){
                 Meeting meeting1 = meetings.get(mNames);
-                dateCollides = isDateWithiRange(meeting1.getTo(), meeting1.getFrom(), meeting.getTo()) &&
-                        isDateWithiRange(meeting1.getTo(),meeting1.getFrom(), meeting.getFrom());
-                if(dateCollides){
-                    collisionMeetingName = meeting1.getName();
+                if(isDateWithiRange(meeting1.getTo(), meeting1.getFrom(), meeting.getTo() ) || meeting1.getTo().equals(meeting.getTo())){
+                    meetingsWithcollisonTime.add(meeting1.getName());
+                }
+                if (isDateWithiRange(meeting1.getTo(),meeting1.getFrom(), meeting.getFrom()) || meeting1.getFrom().equals(meeting.getFrom())){
+                    meetingsWithcollisonTime.add(meeting1.getName());
                 }
             }
-            if(!dateCollides){
+
+            if(meetingsWithcollisonTime.isEmpty()){
                 meetings.put(meeting.getName(), meeting);
             } else {
-                System.out.println("Meeting time collides with meeting Name: "+ collisionMeetingName);
+                System.out.println("Meeting time collides with meeting Name: "+ meetingsWithcollisonTime.get(0));
             }
         } else {
             System.out.println("Meeting with the above name already exists, please create a meeting with separate name");
